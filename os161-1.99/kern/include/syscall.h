f6b8e420faf244d794a5c9eafe5fa1ad600e470f
masaar <masaar@student.cs.uwaterloo.ca>
2019-02-21 13:43:59 -0500

    changed syscall.h, built once

--- os161-1.99/kern/include/syscall.h
+++ os161-1.99/kern/include/syscall.h
@@ -66,12 +66,12 @@ int sys___time(userptr_t user_seconds, userptr_t user_nanoseconds);
 #ifdef UW
 int sys_write(int fdesc,userptr_t ubuf,unsigned int nbytes,int *retval);
 void sys__exit(int exitcode);
+int sys_getpid(pid_t *retval);
+int sys_waitpid(pid_t pid, userptr_t status, int options, pid_t *retval);
+#endif // UW
+
 #ifdef OPT_A2
 int sys_fork(struct trapframe *tf, pid_t * retval);
 #endif // OPT_A2
-int sys_getpid(pid_t *retval);
-int sys_waitpid(pid_t pid, userptr_t status, int options, pid_t *retval);
-
-#endif // UW
 
 #endif /* _SYSCALL_H_ */
